bd06c8dbe9aec91ba3f9b3e037e7971deb91ccf2
Vibhav Ahuja <vibhav.ahuja@netsmartz.local>
2025-07-25 12:42:35 +0530

    AI News

--- src/langgraphagenticai/graph/graph_builder.py
+++ src/langgraphagenticai/graph/graph_builder.py
@@ -5,7 +5,7 @@ from src.langgraphagenticai.nodes.basic_chatbot_node import BasicChatbotNode
 from src.langgraphagenticai.tools.search_tool import get_tools,create_tool_node
 from langgraph.prebuilt import tools_condition,ToolNode
 from src.langgraphagenticai.nodes.chatbot_with_Tool_node import ChatbotWithToolNode
-
+from src.langgraphagenticai.nodes.ai_news_node import AINewsNode
 class GraphBuilder:
     def __init__(self,model):
         self.llm = model
@@ -44,6 +44,19 @@ class GraphBuilder:
         self.graph_builder.add_conditional_edges('chatbot',tools_condition)
         self.graph_builder.add_edge('tools','chatbot')
 
+
+    def ai_news_builder_graph(self):
+        ai_news_node = AINewsNode(self.llm)
+
+        self.graph_builder.add_node("fetch_news",ai_news_node.fetch_news)
+        self.graph_builder.add_node("summarize_news", ai_news_node.summarize_news)
+        self.graph_builder.add_node("save_result", ai_news_node.save_result)
+
+        self.graph_builder.set_entry_point("fetch_news")
+        self.graph_builder.add_edge("fetch_news","summarize_news")
+        self.graph_builder.add_edge("summarize_news","save_result")
+        self.graph_builder.add_edge("save_result", END)
+
     def setup_graph(self, usecase: str):
         """
         Sets up the graph for the selected use case.
@@ -52,5 +65,7 @@ class GraphBuilder:
             self.basic_chatbot_build_graph()
         if usecase == 'Chatbot With Web':
             self.basic_chatbot_build_graph()
+        if usecase == "AI News":
+            self.ai_news_builder_graph()
 
         return self.graph_builder.compile()
